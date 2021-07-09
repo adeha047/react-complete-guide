73783eba32618bfce912fc7a05fcba18baaf642b
adeha047 <dehaven7777@gmail.com>
2021-07-09 13:39:06 -0400

    added new updates

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,16 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
-    return <h2> Expense item </h2>
+   return(
+   <div className="expense-item">
+    <h2> August 2021</h2>
+    <div className="expense-item_description">
+        <h1>Car Insurance </h1>
+        <div className="expense-item_price">28.99$</div>
+    </div>
+
+    </div>
+   );
 }
 
 export default ExpenseItem;
